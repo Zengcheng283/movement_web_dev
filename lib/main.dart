25a4e7c9d8bacf83ee8c4f07fe732e9bbf60fcd0
zengcheng <1265207567@qq.com>
2022-09-20 23:26:38 +0800

    添加主页与登录页，在进入主页之前首先进入登录页，在登录页成功登陆后再进入主页

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,24 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'LoginPage.dart';
+import 'HomePage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Startup Name Generator',
-      home: Scaffold(
-        body: Center(
-          // child: Text("HelloWorld!"), // 注释掉下面一行以及以下所有类，启用这行为HelloWorld
-          child: RandomWords(),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false, // 不显示右上角的 debug
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          actions: <Widget>[
-            IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
-          ],
-        ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return const Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
+        // 注册路由表
+        routes: {
+          "/": (BuildContext context) => new LoginPage(title: "登录"), // 首页路由
+          "/Home": (BuildContext context) => new HomePage(title: "主页"), // 主页路由
         });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-        MaterialPageRoute<void>(
-          builder: (BuildContext context) {
-            final Iterable<ListTile> tiles = _saved.map(
-                  (WordPair pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              },
-            );
-            final List<Widget> divided = ListTile
-                .divideTiles(
-              context: context,
-              tiles: tiles,
-            )
-                .toList();
-
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Saved Suggestions'),
-              ),
-              body: ListView(children: divided),
-            );
-          },
-        ),
-    );
   }
 }
